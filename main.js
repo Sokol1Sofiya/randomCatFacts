01a7befa21fbfc3345b11c8c3d570214eaa40e87
Just <52918574+Moneyjuststickwiththeboy@users.noreply.github.com>
2024-02-02 08:28:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-
 const { Octokit } = require('@octokit/rest');
-
 
 const gitTokens = ['token1', 'token2']
 
-
 const gitNames = ['name1', 'name2']
-
 
 
 
@@ -15,14 +11,11 @@ function generateUniqueName() {
 }
 
 
-
 async function pushRandomCatName(repoOwner, token) {
-    
     const octokit = new Octokit({ auth: token });
 
     try {
 
-       
         const branchFilesData = await octokit.rest.repos.getContent({
             owner: repoOwner,
             repo: 'randomCatFacts',
@@ -31,24 +24,19 @@ async function pushRandomCatName(repoOwner, token) {
 
         console.log(branchFilesData, 'branchFilesData')
 
-        
         const catsResponse = await fetch("https://catfact.ninja/fact");
         const { fact } = await catsResponse.json();
 
-        
         let randomName = generateUniqueName();
 
-        
         const isAlreadyFileNameExist = !!branchFilesData.data.find((user) => {
             return user.path === randomName;
         });
 
-        
         if (isAlreadyFileNameExist) {
             pushRandomCatName(repoOwner, token);
         }
 
-        
         await octokit.rest.repos.createOrUpdateFileContents({
             owner: repoOwner,
             repo: 'randomCatFacts',
@@ -63,12 +51,10 @@ async function pushRandomCatName(repoOwner, token) {
     }
 }
 
-
 function startExecution() {
     gitTokens.forEach((token, index) => {
         pushRandomCatName(gitNames[index], token);
     })
 }
-
 
 startExecution();
